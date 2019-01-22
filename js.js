78489a5349bec354887b9e68cc2ab609cac606d0
Sebastian Persson <sebastian.persson.91@gmail.com>
2019-01-22 16:31:09 +0100

    Added css

---
+++ js.js
@@ -0,0 +1,29 @@
+$('#wrapper').css({
+    width: '100%',
+    height: '98vh',
+    display: 'flex'
+})
+
+$('#leftSection').css({
+    width: '50%',
+    height: '100%'
+})
+
+$('#rightSection').css({
+    width: '50%',
+    height: '100%'
+})
+
+$('.iconSize').css({
+    width: '25px',
+    height: '25px'
+
+});
+
+$('.infoWrapper').css({
+    display: 'flex',
+    width: '500px',
+    marginTop: '20px',
+    color: 'white'
+});
+

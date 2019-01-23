e4649e7185768b1b612e8aeabdfe38f8a00bc412
berggrenjennie <berggrenjennie@gmail.com>
2019-01-23 13:09:00 +0100

    more css

--- js.js
+++ js.js
@@ -1,19 +1,50 @@
+
+
 $('#wrapper').css({
     width: '100%',
     height: '100vh',
     display: 'flex',
     flexDirection: 'column'
-})
+});
 
 $('#leftSection').css({
-    width: '50%',
-})
+    width: '50%'
+});
 
 $('#rightSection').css({
     width: '50%',
     zIndex: "1",
     backgroundColor: "white"
-})
+});
+
+$('.inputs').css({
+  backgroundColor: '#fff',
+  border: '1px solid #e6ecf0',
+  borderRadius: '3px',
+  width: '200px',
+  height: '45px',
+  padding: '10px',
+  marginRight: "10px"
+
+});
+$('#logIn1').css({
+  borderRadius: '35px',
+  backgroundColor: '#fff',
+  border: '1px solid #1da1f2',
+  color: '#1da1f2',
+  width: '100px',
+  height: '45px',
+  fontWeight: 'bold',
+  outline: 'none',
+}).hover(function(){
+  $(this).css("background-color", "aliceBlue");
+
+}, function(){
+  $(this).css("background-color", "white");
+});
+
+
+
 
 $('.iconSize').css({
   fontSize: "35px",
